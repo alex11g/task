e7972286dc9c9993eac4acaa0bec29b6f589a1e2
alex11g <duranmateo50@gmail.com>
2023-03-02 22:20:33 -0500

    version2

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const targetas = document.getElementById("targetas")
+
+function targetasDos(array) {
+   return `
+<div class="card mt-4" >
+                <img src="${array.image} " class="card-img-top mt-2" alt="foto-targeta">
+                <div class="card-body">
+                    <h5 class="card-title text-center">${array.name} </h5>
+                    <p class="card-text ">${array.description} </p>
+                    <p class="precio">Price <strong class="ms-2">$ ${array.price} </strong> </p>
+                    <a href="./info.html" class="btn btn-primary precio2 ">Go somewhere</a>
+                </div>
+ </div>
+`
+} 
+
+function subirTargeta (array, elemento){
+    let template = ''
+    for( let valor of array ){
+        template += targetasDos( valor )
+    }
+    elemento.innerHTML = template
+}
+subirTargeta(data.events,targetas)
+
+
